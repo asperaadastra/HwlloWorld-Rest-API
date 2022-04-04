d218e88ccb991b349014ba855ab4b7ecef983eab
asperaadastra <96790989+asperaadastra@users.noreply.github.com>
2022-04-04 12:49:04 +0500

    .

--- echoBot/main.go
+++ echoBot/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -20,15 +21,67 @@ func main() {
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
-
+	type M map[string]interface{}
+	var UserData []M
+	var Connections []M
+	test := M{"User1": 1221, "User1 Text": "", "User2": 2112, "User2 Text": ""}
+	Connections = append(Connections, test)
 	for update := range updates {
 		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			log.Printf("[%s] %s", update.Message.From.ID, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+			newUserMsg := M{"username": update.Message.From.ID, "Text": update.Message.Text}
+			// UserData = append(UserData, newUserMsg)
+			if len(UserData) != 0 {
+				for i := 0; i <= (len(UserData) - 1); i += 1 {
+					if newUserMsg["Text"] == UserData[i]["Text"] {
+						fmt.Println(UserData[i]["Text"])
+						fmt.Println(newUserMsg["Text"])
+						Connection := M{"User1": newUserMsg["username"], "User1 Text": newUserMsg["Text"], "User2": UserData[i]["username"], "User2 Text": UserData[i]["Text"]}
 
-			bot.Send(msg)
+						Connections = append(Connections, Connection)
+						fmt.Println(Connections)
+
+					}
+				}
+				if len(Connections) == 0 {
+					UserData = append(UserData, newUserMsg)
+				} else if newUserMsg["username"] != Connections[len(Connections)-1]["User1"] {
+					UserData = append(UserData, newUserMsg)
+				}
+
+				// for i := 0; i <= (len(UserData) - 1); i += 1 {
+				// 	fmt.Println(i)
+				// }
+				// UserData = append(UserData, newUserMsg)
+
+			} else {
+				UserData = append(UserData, newUserMsg)
+			}
+			if len(Connections) > 0 {
+				var User1 int = int64(Connections[0]["User1"].(int))
+				var User2 int = int64(Connections[0]["User2"].(int))
+				if update.Message.Chat.ID == User1{
+
+					// a := []interface{}{1, 2, 3, 4, 5}
+					// b := make([]int, len(a))
+					// for i := range a {
+					// 		b[i] = a[i].(int)
+					// }
+					// fmt.Println(a, b)
+
+					msg := tgbotapi.NewMessage(User2, Connections[0]["User1 Text"].(string))
+					bot.Send(msg)
+					// msg.ReplyToMessageID = update.Message.MessageID
+				} else if update.Message.Chat.ID == User2 {
+					msg := tgbotapi.NewMessage(User1, Connections[0]["User2 Text"].(string))
+					bot.Send(msg)
+					// msg.ReplyToMessageID = update.Message.MessageID
+				}else{
+					fmt.Println("No Connected Users Yet")
+				}
+			}
+
 		}
 	}
 }
